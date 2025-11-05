3002851abe20c9207a020abe303de1d8c2c4a1aa
yeong <rlatjdud6573@naver.com>
2025-11-06 00:08:46 +0900

    feat: Color, ThemeData 설정

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 ### 기능 구현 순서
 
 - [x] First Commit: 대학 스포츠 동아리 관리 어플리케이션 초기 설정
-- [ ] feat: Color, ThemeData 설정
+- [x] feat: Color, ThemeData 설정
 - [ ] feat: Router, Riverpod 설정
 
 - [ ] feat: Navigation Bar (Home, Post, Upload, Notification, My) 구현
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:yeong_dongari_nest/constants/colors.dart';
+import 'package:yeong_dongari_nest/constants/sizes.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,6 +14,178 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Yeong Dongari Nest',
       debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        useMaterial3: false,
+        primaryColor: AppColors.primary,
+        colorScheme: ColorScheme.light(
+          primary: AppColors.primary,
+          secondary: AppColors.secondary,
+          surface: AppColors.neutral100,
+          error: AppColors.error,
+          onPrimary: Colors.white,
+          onSecondary: Colors.white,
+          onSurface: AppColors.neutral800,
+          onError: Colors.white,
+        ),
+        splashColor: Colors.transparent,
+        scaffoldBackgroundColor: Colors.white,
+        appBarTheme: AppBarTheme(
+          backgroundColor: Colors.white,
+          foregroundColor: AppColors.neutral800,
+          elevation: 0,
+        ),
+        bottomAppBarTheme: BottomAppBarThemeData(color: AppColors.neutral100),
+        inputDecorationTheme: InputDecorationTheme(
+          hintStyle: Theme.of(
+            context,
+          ).textTheme.titleMedium!.copyWith(color: AppColors.neutral400),
+          enabledBorder: InputBorder.none,
+          focusedBorder: InputBorder.none,
+        ),
+        tabBarTheme: TabBarThemeData(
+          labelStyle: Theme.of(context).textTheme.titleSmall,
+          unselectedLabelStyle: Theme.of(context).textTheme.titleSmall,
+          unselectedLabelColor: AppColors.neutral500,
+          labelColor: Colors.white,
+          indicatorColor: Colors.transparent,
+        ),
+        textTheme: TextTheme(
+          titleLarge: TextStyle(
+            fontFamily: "PretendardSemibold",
+            fontSize: Sizes.d20,
+            letterSpacing: -0.025,
+            color: AppColors.neutral800,
+          ),
+          titleMedium: TextStyle(
+            fontFamily: "PretendardSemibold",
+            fontSize: Sizes.d18,
+            letterSpacing: -0.025,
+            color: AppColors.neutral800,
+          ),
+          titleSmall: TextStyle(
+            fontFamily: "PretendardSemibold",
+            fontSize: Sizes.d16,
+            letterSpacing: -0.025,
+            color: AppColors.neutral800,
+          ),
+          bodyLarge: TextStyle(
+            fontFamily: "PretendardMedium",
+            fontSize: Sizes.d18,
+            letterSpacing: -0.025,
+            color: AppColors.neutral800,
+          ),
+          bodyMedium: TextStyle(
+            fontFamily: "PretendardMedium",
+            fontSize: Sizes.d16,
+            letterSpacing: -0.025,
+            color: AppColors.neutral800,
+          ),
+          bodySmall: TextStyle(
+            fontFamily: "PretendardMedium",
+            fontSize: Sizes.d14,
+            letterSpacing: -0.025,
+            color: AppColors.neutral800,
+          ),
+          labelLarge: TextStyle(
+            fontFamily: "PretendardSemibold",
+            fontSize: Sizes.d14,
+            letterSpacing: -0.025,
+            color: AppColors.neutral800,
+          ),
+          labelSmall: TextStyle(
+            fontFamily: "PretendardMedium",
+            fontSize: Sizes.d12,
+            letterSpacing: -0.025,
+            color: AppColors.neutral800,
+          ),
+        ),
+      ),
+      darkTheme: ThemeData(
+        useMaterial3: false,
+        primaryColor: AppColors.primary,
+        colorScheme: ColorScheme.dark(
+          primary: AppColors.primary,
+          secondary: AppColors.secondary,
+          surface: AppColors.neutral900,
+          error: AppColors.error,
+          onPrimary: Colors.white,
+          onSecondary: Colors.white,
+          onSurface: AppColors.neutral200,
+          onError: Colors.white,
+        ),
+        splashColor: Colors.transparent,
+        scaffoldBackgroundColor: Colors.black,
+        appBarTheme: AppBarTheme(
+          backgroundColor: Colors.black,
+          foregroundColor: AppColors.neutral200,
+          elevation: 0,
+        ),
+        bottomAppBarTheme: BottomAppBarThemeData(color: AppColors.neutral800),
+        inputDecorationTheme: InputDecorationTheme(
+          hintStyle: Theme.of(
+            context,
+          ).textTheme.titleMedium!.copyWith(color: AppColors.neutral500),
+          enabledBorder: InputBorder.none,
+          focusedBorder: InputBorder.none,
+        ),
+        tabBarTheme: TabBarThemeData(
+          labelStyle: Theme.of(context).textTheme.titleSmall,
+          unselectedLabelStyle: Theme.of(context).textTheme.titleSmall,
+          unselectedLabelColor: AppColors.neutral500,
+          labelColor: Colors.white,
+          indicatorColor: Colors.transparent,
+        ),
+        textTheme: TextTheme(
+          titleLarge: TextStyle(
+            fontFamily: "PretendardSemibold",
+            fontSize: Sizes.d20,
+            letterSpacing: -0.025,
+            color: AppColors.neutral200,
+          ),
+          titleMedium: TextStyle(
+            fontFamily: "PretendardSemibold",
+            fontSize: Sizes.d18,
+            letterSpacing: -0.025,
+            color: AppColors.neutral200,
+          ),
+          titleSmall: TextStyle(
+            fontFamily: "PretendardSemibold",
+            fontSize: Sizes.d16,
+            letterSpacing: -0.025,
+            color: AppColors.neutral200,
+          ),
+          bodyLarge: TextStyle(
+            fontFamily: "PretendardMedium",
+            fontSize: Sizes.d18,
+            letterSpacing: -0.025,
+            color: AppColors.neutral200,
+          ),
+          bodyMedium: TextStyle(
+            fontFamily: "PretendardMedium",
+            fontSize: Sizes.d16,
+            letterSpacing: -0.025,
+            color: AppColors.neutral200,
+          ),
+          bodySmall: TextStyle(
+            fontFamily: "PretendardMedium",
+            fontSize: Sizes.d14,
+            letterSpacing: -0.025,
+            color: AppColors.neutral200,
+          ),
+          labelLarge: TextStyle(
+            fontFamily: "PretendardSemibold",
+            fontSize: Sizes.d14,
+            letterSpacing: -0.025,
+            color: AppColors.neutral200,
+          ),
+          labelSmall: TextStyle(
+            fontFamily: "PretendardMedium",
+            fontSize: Sizes.d12,
+            letterSpacing: -0.025,
+            color: AppColors.neutral200,
+          ),
+        ),
+      ),
     );
   }
 }
