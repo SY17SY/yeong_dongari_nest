3c44d498391867d17c65db1063bb5a076104d43d
yeong <rlatjdud6573@naver.com>
2025-11-11 22:20:25 +0900

    feat: PostDetailScreen router 추가

--- lib/router.dart
+++ lib/router.dart
@@ -3,6 +3,8 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:yeong_dongari_nest/common/main_navigation/view/main_navigation_shell.dart';
 import 'package:yeong_dongari_nest/feature/home/view/home_screen.dart';
+import 'package:yeong_dongari_nest/feature/post/model/post_model.dart';
+import 'package:yeong_dongari_nest/feature/post/view/post_detail_screen.dart';
 import 'package:yeong_dongari_nest/feature/upload/view/upload_screen.dart';
 
 final routerProvider = Provider((ref) {
@@ -16,6 +18,17 @@ final routerProvider = Provider((ref) {
           final tab = state.pathParameters["tab"]!;
           return MainNavigationShell(tab: tab);
         },
+        routes: [
+          GoRoute(
+            name: PostDetailScreen.routeName,
+            path: PostDetailScreen.routeUrl,
+            builder: (context, state) {
+              final postId = state.pathParameters["postId"]!;
+              final post = state.extra as PostModel;
+              return PostDetailScreen(postId: postId, post: post);
+            },
+          ),
+        ],
       ),
       GoRoute(
         name: UploadScreen.routeName,
