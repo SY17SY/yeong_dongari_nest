f88367c6c2d35e332476550ac103148bc0efdffa
yeong <rlatjdud6573@naver.com>
2025-11-13 00:13:22 +0900

    chore: firebase 설치, 초기화

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:yeong_dongari_nest/constants/colors.dart';
 import 'package:yeong_dongari_nest/constants/sizes.dart';
 import 'package:yeong_dongari_nest/feature/setting/view_model/setting_vm.dart';
+import 'package:yeong_dongari_nest/firebase_options.dart';
 import 'package:yeong_dongari_nest/router.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
   final sharedPreferences = await SharedPreferences.getInstance();
 
